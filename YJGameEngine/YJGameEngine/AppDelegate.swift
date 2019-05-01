47e1a8951d054a2594fa332a734f900e33d850df
Yujie Liu <yujiel@fb.com>
2019-05-01 11:30:03 -0700

    Add initial files

---
+++ YJGameEngine/YJGameEngine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YJGameEngine
+//
+//  Created by Yujie Liu on 5/1/19.
+//  Copyright © 2019 Yujie Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ YJGameEngine/YJGameEngineTests/YJGameEngineTests.swift
@@ -0,0 +1,34 @@
+//
+//  YJGameEngineTests.swift
+//  YJGameEngineTests
+//
+//  Created by Yujie Liu on 5/1/19.
+//  Copyright © 2019 Yujie Liu. All rights reserved.
+//
+
+import XCTest
+@testable import YJGameEngine
+
+class YJGameEngineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
